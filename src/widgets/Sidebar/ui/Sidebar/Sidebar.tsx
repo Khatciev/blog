2b20618a0fbd01238dd51a2997ef3136c29d5fe3
Khatciev <hacievasdf@mail.ru>
2023-04-22 23:12:02 +0300

    fix screenshots, add jobs from main yml

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwither } from 'shared/ui/ThemeSwitcher';
 import LangSwitcher from 'shared/ui/LangSwither/LangSwither';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -13,7 +14,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-
+    const { t } = useTranslation();
     const onToggleCollapsed = () => {
         setCollapsed((prev) => !prev);
     };
@@ -27,7 +28,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggleCollapsed}
             >
-                toggle
+                {t('Переключать')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwither />
